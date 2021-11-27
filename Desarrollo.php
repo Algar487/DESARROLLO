7a89db8b1967c276a9511879cbdc228724cfc3ce
alejandro.ruiga <44670069+Algar487@users.noreply.github.com>
2021-11-27 15:53:49 +0100

    g

--- Desarrollo.php
+++ Desarrollo.php
@@ -10,8 +10,15 @@ function convertirHoraAMinutos ($hora){
     $minutos = explode(":", $hora);
     $hours=$minutos[0];
     $minutes=$minutos[1];
-    $resultado=$minutes+$hours*60;
-    return $resultado;
+    $hours = (int)$hours;
+    $minutes = (int)$minutes;
+    if($hours<23&&$hours>=0&&$minutes<=60&&$minutes>=0;{
+        $resultado=$minutes+$hours*60;
+        return $resultado;
+    }
+    else{
+        return false;
+    }
 }
 
 echo convertirHoraAMinutos("11:30");
