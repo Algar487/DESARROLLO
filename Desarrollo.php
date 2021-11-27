0746be5b4ca2cfda68fe243faa78eb1ddf385b1a
alejandro.ruiga <44670069+Algar487@users.noreply.github.com>
2021-11-27 15:41:22 +0100

    h

--- Desarrollo.php
+++ Desarrollo.php
@@ -2,10 +2,17 @@
 
 $horario=['10:00-13:30','16:30-20:30'];
 $ocupacion=['11:30-12:30','12:31-13:30','16:30-18:00'];
+$hora  = "11:30";
+
 
 function convertirHoraAMinutos ($hora){
-
-    
+    $minutos = explode(":", $hora);
+    $hours=$minutos[0];
+    $minutes=$minutos[1];
+    $resultado=$hours+$minutes*60;
+    return $resultado;
 }
+
+echo convertirHoraAMinutos("11:30");
 
 
