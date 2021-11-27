4f0700bff1181ee84f411078e9d9f258e9c1b630
alejandro.ruiga <44670069+Algar487@users.noreply.github.com>
2021-11-27 15:56:15 +0100

    f

--- Desarrollo.php
+++ Desarrollo.php
@@ -3,7 +3,7 @@
 $horario=['10:00-13:30','16:30-20:30'];
 $ocupacion=['11:30-12:30','12:31-13:30','16:30-18:00'];
 $hora= "11:30";
-$tramo= "11:30-12:30"
+$tramo= "11:30-12:30";
 
 
 function convertirHoraAMinutos ($hora){
